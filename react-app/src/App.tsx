e0eaf1476199883ccbbe455fe43730dcff491a27
Mark R. <51971308+qonTesq@users.noreply.github.com>
2026-02-03 21:26:57 -0500

    style: adds dark theme, responsive styles and layout

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,16 +7,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
-          <img src={nevah} className="nevah" alt="Never Gonna Give You Up" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <main>
+        <div className="image-container">
+          <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+            <img src={nevah} className="nevah" alt="Never Gonna Give You Up" />
+          </a>
+        </div>
+        <div className="card">
+          <button
+            onClick={() => setCount((count) => count + 1)}
+            className="count-btn"
+          >
+            count is {count}
+          </button>
+        </div>
+      </main>
     </>
   );
 }
